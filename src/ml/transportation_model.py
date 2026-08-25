70e39779d26a486312d21394286360425ad58ce7
SatyamVerse <linkedinsatyam2006@gmail.com>
2026-08-26 00:31:53 +0530

    Add RandomForestRegressor import to transportation model

--- src/ml/transportation_model.py
+++ src/ml/transportation_model.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
 print("Campus X ML Module")
 print("Loading transportation dataset...")
 
